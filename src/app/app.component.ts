77cc8330e754b4d3b4f0ffb19ed36b2f6c3500bf
Raquel Correa <raquelfilosofiamackenzie@gmail.com>
2024-05-22 12:00:58 -0300

    Atualizações progress bar e designs cadatro clientes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { CadastroDeClienteComponent } from './componentes/cadastro-de-cliente/cadastro-de-cliente.component';
 import { SimulacaoComponent } from './componentes/simulacao/simulacao.component';
 import { RodapeComponent } from './componentes/rodape/rodape.component';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CadastroDeClienteComponent,SimulacaoComponent,RodapeComponent],
+  imports: [RouterOutlet,CadastroDeClienteComponent,SimulacaoComponent,RodapeComponent,MatProgressBar],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
